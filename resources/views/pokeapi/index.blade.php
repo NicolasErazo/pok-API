da7db60227ea000c0ea4a10f70a4d07caf181d08
NicolasErazo <nico.lacho@outlook.com>
2022-10-06 17:36:53 -0500

    ok

--- resources/views/pokeapi/index.blade.php
+++ resources/views/pokeapi/index.blade.php
@@ -4,20 +4,18 @@
 
 <div class="container">
     <div class="row justify-content-center">
-        <h6> Observaciones: Solamente se puede buscar por ID menor que 905 y nombre exacto de pokemon</h6>
         <div class="col-md-12 text-center">
             <form action="/home" method="POST" class="d-flex">
                 @csrf
                 <input type="text" class="form-control me-2" name="pokemon" placeholder="Ingrese Id o nombre de Pokemon a buscar...">
                 <button type="submit" class="btn btn-success">Buscar</button>
             </form>
+            <div class="row mt-2">
+                <h6><strong> Observaciones: </strong> Solamente se puede buscar por ID menor que 905 y nombre exacto de pokemon</h6>
+            </div>
             <br>
             <div class="card" style="width: 18rem;">
-                @if(isset($foto))
                 <img class="card-img-top" src="{{ $foto }}" alt="Card image cap">
-                @else
-                <p></p>
-                @endif
                 <div class="card-body">
                     <h5 class="card-title">Nombre: <strong>{{ $name }}</strong></h5>
                     <form action="/home/details" method="POST">
